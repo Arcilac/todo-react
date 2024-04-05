d6f51c73d605557bcdeab26b8af359f03d99508f
arcilac <arcilacamila08@gmail.com>
2024-04-04 19:36:52 -0500

    add existing file

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -2,6 +2,9 @@ import { TodoCounter } from "../TodoCounter"
 import { TodoSearch } from "../TodoSearch"
 import { TodoList } from "../TodoList"
 import { TodoItem } from "../TodoItem"
+import {TodosLoading} from "../TodosLoading"
+import {TodosError} from "../TodosError"
+import {EmptyTodos} from "../EmptyTodos"
 import { CreateTodoButton } from "../CreateTodoButton"
 
 function AppUI({
@@ -22,9 +25,16 @@ return (
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
-        {loading && <p>Loading</p>}
-        {error && <p>Error</p>}
-        {(!loading && searchedTodos.length === 0) && <p>Create your first TODO</p>}
+        {loading && (
+          <>
+          <TodosLoading />
+          <TodosLoading />
+          <TodosLoading />
+          <TodosLoading />
+          </>
+        ) }
+        {error && <TodosError/>}
+        {(!loading && searchedTodos.length === 0) && <EmptyTodos />}
 
         {searchedTodos.map((todo) => (
           <TodoItem
--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -7,21 +7,9 @@ function TodoItem(props) {
     <li className="TodoItem">
       <CompleteIcon completed={props.completed} onComplete={props.onComplete} />
 
-      {/*<span 
-        className={`Icon Icon-check ${props.
-        completed && "Icon-check--active"}`}
-        onClick={props.onComplete}
-        >
-        V
-      </span>*/}
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>{props.text}</p>
       <DeleteIcon onDelete={props.onDelete} />
-      {/*<span 
-        className="Icon Icon-delete"
-        onClick={props.onDelete}
-      >
-        X
-    </span>*/}
+      
     </li>
   )
 }
