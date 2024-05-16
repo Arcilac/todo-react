7da9c696eeaeb19980c1a7ffd30b73a3d581fd48
arcilac <arcilacamila08@gmail.com>
2024-05-16 02:31:24 -0500

    create routes

---
+++ src/routes/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { HashRouter, Route, Routes } from "react-router-dom"
+import { NewTodoPage } from "./new/NewTodoPage"
+import { EditTodoPage } from "./edit/EditTodoPage"
+import { HomePage } from "./home/HomePage"
+
+function App() {
+  
+  return (
+    <HashRouter> 
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/new" element={<NewTodoPage />} />
+        <Route path="/edit/:id" element={<EditTodoPage />} />
+      </Routes>
+    </HashRouter>
+  )
+}
+
+export { App }
