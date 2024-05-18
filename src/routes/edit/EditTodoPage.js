3d8b9372c8b82c5e55a59439cb68b0614eceda71
arcilac <arcilacamila08@gmail.com>
2024-05-17 19:12:10 -0500

    useLocation

--- src/routes/edit/EditTodoPage.js
+++ src/routes/edit/EditTodoPage.js
@@ -1,21 +1,36 @@
 import React from "react"
 import { TodoForm } from "../../ui/TodoForm"
 import { useTodos } from "../useTodos"
-import { useParams } from "react-router-dom"
+import { useLocation, useParams } from "react-router-dom"
 
 function EditTodoPage() {
+    const location = useLocation()
     const params = useParams()
     const id = Number(params.id)
-    const { stateUpdaters } = useTodos()
+
+    const { state, stateUpdaters } = useTodos()
+    const { loading, getTodo } = state
     const { editTodo } = stateUpdaters
 
-  return(
-    <TodoForm 
-        label="Edit TODO"
-        submitText="Edit"
-        submitEvent={(newText) => editTodo( id, newText )} 
-    />
-  )
+    let todoText
+
+    if (location.state?.todo) {
+        todoText = location.state.todo.text
+    } else if(loading){
+         return <p>Loading...</p>
+    } else {
+        const todo = getTodo(id)
+        todoText = todo.text
+    }
+    return(
+        <TodoForm 
+            label="Edit TODO"
+            defaultTodoText={todoText}
+            submitText="Edit"
+            submitEvent={(newText) => editTodo( id, newText )} 
+        />
+      )
+  
 }
 
 export { EditTodoPage }
