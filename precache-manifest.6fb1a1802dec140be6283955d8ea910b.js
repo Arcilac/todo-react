825706ad7cae8a1176b73488e97c73279159249d
arcilac <arcilacamila08@gmail.com>
2024-05-17 19:19:58 -0500

    Updates

---
+++ precache-manifest.6fb1a1802dec140be6283955d8ea910b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c535a93ea3b07660503491b7cb4777a",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "99b7c02e42fc0f516df0",
+    "url": "/todo-react/static/css/main.f70ec1d2.chunk.css"
+  },
+  {
+    "revision": "b39da464af08b024d6c8",
+    "url": "/todo-react/static/js/2.3f3a12b4.chunk.js"
+  },
+  {
+    "revision": "99b7c02e42fc0f516df0",
+    "url": "/todo-react/static/js/main.73fb421e.chunk.js"
+  },
+  {
+    "revision": "cd203911fc85a7f73b39",
+    "url": "/todo-react/static/js/runtime~main.6292bb99.js"
+  },
+  {
+    "revision": "a6ff2ce648ff3421426db7842d40d1d0",
+    "url": "/todo-react/static/media/check.a6ff2ce6.svg"
+  },
+  {
+    "revision": "45d0d71a1f8f92c1da6744303a373163",
+    "url": "/todo-react/static/media/delete.45d0d71a.svg"
+  },
+  {
+    "revision": "0066693d53c2de8bb2956d1bb668c54c",
+    "url": "/todo-react/static/media/edit.0066693d.svg"
+  }
+]);
